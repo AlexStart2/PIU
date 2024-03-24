a6237b5e1ed6d29bdf8e29d15fab8a3364519d1b
AlexStart2 <fairytailend2003@gmail.com>
2024-03-24 17:52:27 +0200

    tema de casa laborator 3

---
+++ ConsoleApp/ConsoleApp/Materie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Materie
+    {
+        string name;
+        public string getName { get { return name; } }
+        public void setName(string _name)
+        {
+            if(_name.Equals("")||_name.Equals(null))
+            {
+                throw new Exception("Name should be defined");
+            }
+            name = _name;
+        }
+
+        public Materie() { }
+
+    }
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -8,10 +8,67 @@ namespace ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+        static void Main()
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+            ClassMaterii test = new ClassMaterii();
+            string option = "";
+            while (!option.Equals("X")){
+                Console.WriteLine("A. Lista materii");
+                Console.WriteLine("B. Adauga materie");
+                Console.WriteLine("C. Sterge materie");
+                Console.WriteLine("F. Gaseste dupa nume");
+                Console.WriteLine("F. Gaseste dupa index");
+                Console.WriteLine("L. Incarca din fisier date");
+                Console.WriteLine("W. Scrie datele in fisier");
+                Console.WriteLine("X. Exit");
+
+                option = Console.ReadLine().ToUpper();
+
+                switch (option)
+                {
+                    case "A": // Afiseaza lista de cursuri
+                        displayCourses(test.list());
+                        break;
+                    case "B": // Adauga un curs
+                        Console.Write("Introduceti numele cursului: ");
+                        test.addCourse();
+                        break;
+                    case "C":
+                        Console.Write("Introduceti ce curs doriti sa eliminati: ");
+                        test.removeCourse();
+                        break;
+                    case "F":
+                        Console.Write("Introduceti numele cursului cautat: ");
+                        displayCourses(test.findByName());
+                        break;
+                    case "I":
+                        Console.Write("Introduceti indexul cursului cautat: ");
+                        Console.WriteLine(test.GetCourse().getName);
+                        break;
+                    case "X": 
+                        Console.WriteLine("Terminare Program");
+                        break;
+                    default:
+                        Console.WriteLine("Optiune gresita");
+                        break;
+                }
+            }
+        }
+
+        static void displayCourses(List<Materie> a)
+        {
+            if (a.Count == 0)
+            {
+                Console.WriteLine("Nu exista cursuri");
+                return;
+            }
+            int count = 0;
+            foreach (Materie materie in a)
+            {
+                count++;
+                Console.WriteLine(count + ". " + materie.getName);
+            }
         }
     }
 }
